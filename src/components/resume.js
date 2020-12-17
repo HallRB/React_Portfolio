a822f943995a1b1861dedaaad7e924ef70ea2ab0
Richard <richardbrianhall@gmail.com>
2020-12-16 18:19:01 -0600

    resume uploaded

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import Resumecard from "./resumecard";
 
 class Resume extends Component {
     render() {
-        return(
-            <div><h1>Resume Page</h1></div>
-        )
-    }
+    return(
+        <div class="cardsall">
+<Resumecard></Resumecard>
+</div>
+    )
+}
 }
 export default Resume
---
+++ src/components/resumecard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Resumeimg from './Assetts/Resume.JPG';
+import "./resume.css";
+
+class Resumecard extends Component {
+    render() {
+        return(
+<div class="container">
+<img class="resuming" alt="resume" src={Resumeimg} />
+</div>
+        )
+    }
+}
+export default Resumecard
