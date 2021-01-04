653e02186dfa42b36d9384c325272f19277933dd
Richard <richardbrianhall@gmail.com>
2021-01-04 05:29:01 -0600

    make it better

--- src/index.js
+++ src/index.js
@@ -15,7 +15,13 @@ ReactDOM.render(
   ,document.getElementById('root')
 );
 
-
+export default function Slideshow(){
+    return (
+        <section className="slideshow">
+            Slideshow
+        </section>
+    )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
