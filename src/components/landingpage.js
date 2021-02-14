9be21546dde60e469b0019c8a54fe17f3aa2a368
Richard <richardbrianhall@gmail.com>
2021-02-14 09:23:27 -0600

    checkpoint

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-import Code1 from "./Codesnips/code1";
+import "../App.css";
 class Landingpage extends Component {
     render() {
     return(
-        <div className="resumecard">
-</div>
+     <div id="landingpagewrap">
+         <div id="landingpageinnerwrap">
+
+     <h2 id="cyve">Choose your viewing experience</h2>
+     <div id="mobilewrap">
+     <input type="submit" value="" id="mobver" className="viewexp"></input>
+     </div>
+     <input type="submit" value="Synthwave" id="synthwav" clasName="viewexp"></input>
+     <div id="modern">
+     <input type="submit" value="Modern" id="mod" className="viewexp"></input>
+     </div>
+     <input type="submit" value="Basic" id="bas" className="viewexp"></input>
+     <input type="submit" value="Retro" id="retro" className="viewexp"></input>
+     </div>
+     </div>
     )
 }
 }
