2b27b60ee4595c50afbbd25c757423ecf4701158
Richard <richardbrianhall@gmail.com>
2021-02-26 01:30:09 -0600

    checkpoint

--- src/components/Retro/components/homeretro.js
+++ src/components/Retro/components/homeretro.js
@@ -19,7 +19,7 @@ class HomeRetro extends Component {
 </div>
 <div id="webbaroutretro">
     <div id="webbarinretro">
-             <p id="homeclassic" class="linksclassic"><Link class="links" id="homeclassic" to="/"></Link></p>
+             <p id="homeretro" class="linksretro"><Link class="links" id="homeretro" to="/"></Link></p>
              <p id="resumeretro" class="linksretro"><Link class="links" id="resumeretro" to="/resumeretroview"></Link></p>
              <p id="projectsretro" class="linksretro"><Link class="links" id="projectsretro" to="/projectsretroview"></Link></p>
              <p id="contactretro" class="linksretro"><Link class="links" id="contactretro" to="/contactretroview"></Link></p>
