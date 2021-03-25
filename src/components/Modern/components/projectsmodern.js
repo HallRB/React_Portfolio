f51ff33efe3021843f51464c234290accde0b23e
Richard <richardbrianhall@gmail.com>
2021-03-24 21:09:52 -0500

    added myart to modern, really messed up project format, needs fix

--- src/components/Modern/components/projectsmodern.js
+++ src/components/Modern/components/projectsmodern.js
@@ -12,6 +12,7 @@ import Book from "./Projects/Book";
 import Meta from "./Projects/Meta";
 import Sellify from "./Projects/sellify";
 import Arcadea from "./Projects/arcadeutopia";
+import Myart from "../../Projects/Myart";
 import {  Header, Navigation } from 'react-mdl';
 import { Link } from 'react-router-dom';
 import "../App.css";
@@ -33,6 +34,9 @@ class Projects extends Component {
             </div>
                 Projects
             <div id="cardsallmodern">
+            <div className="inbetweenmodernl">
+<Myart></Myart>
+</div>
 <div id="inbetweenmodernl">
 <Arcadea></Arcadea>
 </div>
