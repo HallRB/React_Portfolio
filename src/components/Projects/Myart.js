682e8585b31592a10f92f754a80d06598059cef7
Richard <richardbrianhall@gmail.com>
2021-03-24 20:54:38 -0500

    added myart project

---
+++ src/components/Projects/Myart.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Card, CardTitle, CardText, CardActions, CardMenu, IconButton } from 'react-mdl';
+
+class Myart extends Component {
+    render() {
+        return(
+<Card class="cardwrap cardbordermain"  shadow={0} style={{width: '512px', margin: 'auto'}}>
+    <CardTitle style={{height: '275px'}}><img alt="" class="myartmain"></img></CardTitle>
+    <CardText class="cardwrap cardtext" >
+        React Portfolio: You're already here!
+    </CardText>
+    <CardActions border>
+    <a class="deployed" href="https://reactportfoliohallrb.herokuapp.com/">Deployed Page</a>
+        <a class="github" href="https://github.com/HallRB/React_Portfolio">Github</a>
+    </CardActions>
+    <CardMenu style={{color: '#fff'}}>
+        <IconButton name="share" />
+    </CardMenu>
+</Card>  
+        )
+    }
+}
+export default Myart
