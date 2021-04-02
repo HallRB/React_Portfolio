4b5111fe321851ef9357055edca67a37086aa9c4
Richard <richardbrianhall@gmail.com>
2021-04-02 17:07:28 -0500

    skills added to mobile

--- src/components/Mobile/components/homemobile.js
+++ src/components/Mobile/components/homemobile.js
@@ -18,25 +18,21 @@ class HomeMobile extends Component {
      <div id="biocardmobile">
      <div id="paramobilewrap">
      <div id="headshotmobile" alt="Avatar"></div>
-     <p id="paramobile">Richard grew up in Texas during the dawn of the internet age. As a child he spent much time playing 32-bit games on the family's Windows 95 PC. In 6th grade he made his first website and worked to build a PC as part of a collaborative project with his schoolmates. He made personal use of Wordpress and Myspace as they arrived at that time. His particularly enjoyed designing personal layouts by playing with colors, fonts, and music. In High School Richard became a competitive online gamer. In college his focus turned towards the arts including acting, singing, playwriting, and painting. He graduated from the University of Maryland in 4 years, earning two degrees with a 3.4 gpa and 152 credit hours. He received a BA in Theatre and a BA in Classical Languages. After graduation, he moved back to Texas to Austin where he worked in administration for the state, pursuing artistic endeavors in his spare time. After 2 years he found a new opportunity of employment with a concrete manufacturer where, outside of the IT department, he was the only person in their local branch familiar with computers. This challenged him to master the use of various software where his technical skills served multiple departments. In 2020 like many, Richard was part of a mass layoff along with 11K+ other employees. Deciding to find a new career path, Richard combined his linguistic, artistic, and technical skills to become a fledgling, full-stack, web-developer in 2021 after graduating from Trilogy Education's Coding Bootcamp through the University of Texas at Austin.  </p>
+     <p id="paramobile">Richard grew up in Texas during the dawn of the internet age. As a child he spent much time playing 32-bit games on the family's Windows 95 PC. In 6th grade he made his first website and worked to build a PC as part of a collaborative project with his schoolmates. He made personal use of Wordpress and Myspace as they arrived at that time. He particularly enjoyed designing personal layouts by playing with colors, fonts, and music. 
+     </p><p id="paramobile">
+     In High School Richard became a competitive online gamer. In college his focus turned towards the arts including acting, singing, playwriting, and painting. He graduated from the University of Maryland in 4 years, earning two degrees with a 3.4 gpa and 152 credit hours. He received a BA in Theatre and a BA in Classical Languages. After graduation, he moved back to Texas to Austin where he worked in administration for the state, pursuing artistic endeavors in his spare time. After 2 years he found a new opportunity of employment with a concrete manufacturer where, outside of the IT department, he was the only person in their local branch familiar with computers. This challenged him to master the use of various software where his technical skills served multiple departments. 
+     </p><p id="paramobile">
+     In 2020 like many, Richard was part of a mass layoff along with 11K+ other employees. Deciding to find a new career path, Richard combined his linguistic, artistic, and technical skills to become a fledgling, full-stack, web-developer in 2021 after graduating from Trilogy Education's Coding Bootcamp through the University of Texas at Austin.  </p>
      </div>
      </div>
-
  </div>
 
- {/* 
-  <div id="lpiwmoderninner">
-  <div className="center-col">
-<ThemeCard></ThemeCard>
- </div>
-<div id="aboutmemodernimp">
-<Biocard></Biocard>
-</div>
 <p></p>
 <div id="gluediv">
-     <div id="skillsmainwrapclassic">
-       <div id="skillsmainmodern">
-       <div id="skillblockmodern">
+    <h1 id="skillstitle">What do I know?</h1>
+     <div id="skillsmainwrapmobile">
+       <div id="skillsmainmobile">
+       <div id="skillblockmobile">
        <div style={{height: '185px'}}><img alt="" id="skilljs"></img></div>
        <div style={{height: '185px'}}><img alt="" id="skillcss"></img></div>
        <div style={{height: '185px'}}><img alt="" id="skillexpress"></img></div>
@@ -55,8 +51,9 @@ class HomeMobile extends Component {
      </div>
      </div>
      </div>
+
      <p></p>
-<div id="resumemodernimp">
+{/* <div id="resumemodernimp">
 <Resume></Resume>
 </div>
 
