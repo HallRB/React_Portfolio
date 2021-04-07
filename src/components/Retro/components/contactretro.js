eb36fc1b193ec454f510ae0841ac40512df29ed9
Richard <richardbrianhall@gmail.com>
2021-04-06 21:26:49 -0500

    cleaning code replaced class with classname in retro

--- src/components/Retro/components/contactretro.js
+++ src/components/Retro/components/contactretro.js
@@ -17,11 +17,11 @@ class Contact extends Component {
 </div>
 <div id="webbaroutretro">
     <div id="webbarinretro">
-             <p id="homeretro" class="linksretro"><Link class="links" id="homeretro" to="/"></Link></p>
-             <p id="resumeretro" class="linksretro"><Link class="links" id="resumeretro" to="/resumeretroview"></Link></p>
-             <p id="projectsretro" class="linksretro"><Link class="links" id="projectsretro" to="/projectsretroview"></Link></p>
-             <p id="contactretro" class="linksretro"><Link class="links" id="contactretro" to="/contactretroview"></Link></p>
-             <p id="aboutmeretro" class="linksretro"><Link class="links" id="aboutmeretro" to="/aboutmeretroview"></Link></p>
+             <p id="homeretro" className="linksretro"><Link className="links" id="homeretro" to="/"></Link></p>
+             <p id="resumeretro" className="linksretro"><Link className="links" id="resumeretro" to="/resumeretroview"></Link></p>
+             <p id="projectsretro" className="linksretro"><Link className="links" id="projectsretro" to="/projectsretroview"></Link></p>
+             <p id="contactretro" className="linksretro"><Link className="links" id="contactretro" to="/contactretroview"></Link></p>
+             <p id="aboutmeretro" className="linksretro"><Link className="links" id="aboutmeretro" to="/aboutmeretroview"></Link></p>
              </div>
 </div>
 
