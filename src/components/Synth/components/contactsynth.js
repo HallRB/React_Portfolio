81651281b91136c2f034bd3247154300b7713486
Richard <richardbrianhall@gmail.com>
2021-04-12 10:36:28 -0500

    cant get backend contact form done will try later pages werent loading deleted files to contact backend

--- src/components/Synth/components/contactsynth.js
+++ src/components/Synth/components/contactsynth.js
@@ -3,39 +3,8 @@ import "./contactsynth.css";
 import {  Header, Navigation } from 'react-mdl';
 import { Link } from 'react-router-dom';
 import "../App.css";
-import axios from 'axios';
 
 class Contact extends Component {
-
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
-  }
-
-  handleSubmit(e){
-    e.preventDefault();
-    axios({
-      method: "POST", 
-      url:"http://localhost:3002/send", 
-      data:  this.state
-    }).then((response)=>{
-      if (response.data.status === 'success') {
-        alert("Message Sent."); 
-        this.resetForm()
-      } else if (response.data.status === 'fail') {
-        alert("Message failed to send.")
-      }
-    })
-  }
-
-  // resetForm(){
-  //   this.setState({name: ‘’, email: ‘’, message: ‘’})
-  // }
 
       render() {
         
